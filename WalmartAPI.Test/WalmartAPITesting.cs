40499b07dc40b0814f88ac8857decf3f1f38220c
Eli Ekstein <eli@jafgifts.com>
2016-08-08 10:50:40 -0400

    Testing Added

---
+++ WalmartAPI.Test/WalmartAPITesting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WalmartAPI.Test
+{
+    public class WalmartAPITesting
+    {
+        [Fact(DisplayName ="Xunit is working")]
+        public void test()
+        {
+            string sut = "tst";
+            Assert.Equal("tst", sut);
+        }
+    }
+}
